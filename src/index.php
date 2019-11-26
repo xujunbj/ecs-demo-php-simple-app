d36fd17aa6ea3ee6e65e8826fa45ea02d2587b7f
xujun-at-948874832909 <xujunaws@amazon.com>
2019-11-26 01:32:39 +0000

    minor update

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <!--<style>body {margin-top: 40px; background-color: #333;}</style>-->
-        <style>body {margin-top: 40px; background-color: #0000FF;}</style>
+        <style>body {margin-top: 40px; background-color: #CCC;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
@@ -18,7 +18,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS. Fantastic!</p>
+                <p>Your PHP application is now running on a container in Amazon ECS. Enjoy it!</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
